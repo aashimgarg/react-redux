fd64f1a0087e94268a0a018c0d62d9d8696306fe
aashimgarg <18bcs001@smvdu.ac.in>
2020-06-07 09:36:58 +0530

    feat(react-redux) added the reducers) :tada:

---
+++ src/redux/cakes/cakeReducers.js
@@ -0,0 +1,15 @@
+import { BUY_CAKE } from './cakeTypes'
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const cakeReducer = (state = initialState, action) =>{
+    switch (action.type) {
+        case BUY_CAKE: return {
+            numOfCakes: state.numOfCakes-1
+        }
+    }
+}
+
+export default cakeReducer;
