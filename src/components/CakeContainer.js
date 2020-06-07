b4b9782e13dc3004de5f16af52453dfc712e6fd2
aashimgarg <18bcs001@smvdu.ac.in>
2020-06-07 09:02:12 +0530

    added the redux packages

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <h2>Number of Cakes</h2>
+            <button> Submit </button>
+        </div>
+    )
+}
+
+export default CakeContainer;
