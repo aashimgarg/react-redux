4ec0a67bb6c882b83aeb0981a7be7ff242d6d4a8
aashimgarg <18bcs001@smvdu.ac.in>
2020-06-09 14:22:57 +0530

    applied the middleware console logger :hammer:

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer'
+import logger from 'redux-logger'
 
 // Using the ES6 import syntax
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware( logger ));
 
 // createStore method accepts a reducer function as its parametre.
 // For this we have rootReducer so we have passed it as a parametre 
